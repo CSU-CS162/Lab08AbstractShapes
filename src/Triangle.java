9ece1c6fd6a0f2278c72e7975a4b45ba9f9c11e1
Emilie <98420041+themilkprince@users.noreply.github.com>
2022-06-27 14:25:22 -0600

    Create Triangle.java

---
+++ src/Triangle.java
@@ -0,0 +1,12 @@
+//This is the class that you will be finishing. You need to implement the area(), perimeter(), and toString()
+
+public class Triangle extends Shape {
+    double length;
+    double width;
+    double third;
+    public Triangle(double length, double width, double third){
+        this.length = length;
+        this.width = width;
+        this.third = third;
+    }
+}
